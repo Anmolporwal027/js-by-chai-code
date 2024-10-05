b10c5f0d10d87698e11ff4b733e31574f8ecc64d
Anmolporwal027 <anmolporwal027@gmail.com>
2024-10-05 10:41:38 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144552
+let accountEmail = "Anmol@gmail.com"
+var accountPassword = "1324"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "java@gmail.com"
+accountPassword = "1234"
+accountCity = "jodhpur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "Anmol"
+let age = 18
+let isLoggedin = true
+let points = 2.33
+
+// number => 2 to the power 53
+// bigInt 
+// string => ""
+// boolean => true or false
+// null => standalone value -> empty value assign ho jati hai
+// undefined => when value is not assigned it is undefined 
+
+console.log(typeof "anmol");
+console.log(typeof age);
+
