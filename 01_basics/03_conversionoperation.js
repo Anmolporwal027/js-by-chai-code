475abcc6d319bde01b9e33846756543b282485c8
anmolporwal027@gmail.com <anmolporwal027@gmail.com>
2024-10-15 09:36:48 +0530

    operations with conversions

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,28 +1,62 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);//to convert string to number
+// let valueInNumber = Number(score);//to convert string to number
 // console.log(typeof valueInNumber);
-console.log(valueInNumber);//Nan(not a number) if numbers and alphabet both are on same variable
+// console.log(valueInNumber);//Nan(not a number) if numbers and alphabet both are on same variable
 
 // "33" => 33
 // "33abs" => Nan
 // true => 1; false => 0
 
-let isLoggedin = "hello"
-let booleanisLoggedin = Boolean(isLoggedin)
-console.log(booleanisLoggedin); 
+// let isLoggedin = "hello"
+// let booleanisLoggedin = Boolean(isLoggedin)
+// console.log(booleanisLoggedin); 
 
 // 0-> false and 1-> true
 // "" => false
 // "hello" => true
 
-let someNumber = false
-let character = String(someNumber);
-console.log(character);
+// let someNumber = false
+// let character = String(someNumber);
+// console.log(character);
 
 // 33 => "33" //string bann jata hai number
 // false => "false"
 // 5.5 => "5.5"
+
+// *********************Operations***************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(1 + "2");//12
+// console.log("2" + 1);//21
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
