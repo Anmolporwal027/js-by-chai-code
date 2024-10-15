96a41a687651c9bd43372832ff27dc30b22d304e
anmolporwal027@gmail.com <anmolporwal027@gmail.com>
2024-10-15 10:48:03 +0530

    comparison operator

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("0" > 1);
+
+// Avoid Below types of conversions
+console.log(null > 0);
+console.log(null == 0); //equality check uses null as a 1 
+console.log(null >= 0);//comparison operator use null as 0
+console.log(null < 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+
+
+// === (strictly check krta hai datatypes ko bhi)
+console.log("2" === 2);
+
+
+
+
+
+
