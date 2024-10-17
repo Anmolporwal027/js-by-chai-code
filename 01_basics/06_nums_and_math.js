76b80753c0907eff2c208014549d7b350d910610
anmolporwal027@gmail.com <anmolporwal027@gmail.com>
2024-10-17 09:00:11 +0530

    maths and numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,42 @@
+const num = 50
+// console.log(num);
+
+const value = new Number(400)
+// console.log(value);
+
+// console.log(value.toString());
+// console.log(value.toString().length);
+
+const otherval = 123.89232
+// console.log(otherval.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));          
+
+
+
+// +++++++++++++++++++++++++ Maths +++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));//jo pas me hai vo choose krega
+// console.log(Math.ceil(4.2));//higher value choose krega
+// console.log(Math.floor(4.9));//lower value use krega
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(1,2,3,4));
+
+console.log(Math.random());
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+// console.log(Math.random() * (max - min + 1))
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+console.log(Math.floor(32));
+
+
+
+
